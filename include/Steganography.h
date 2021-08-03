c70035c398143be6eae55c7779988e5eb01fcff1
noctera <julian.jaksch@gmx.de>
2021-08-04 00:10:36 +0200

    added comments

--- include/Steganography.h
+++ include/Steganography.h
@@ -11,8 +11,8 @@ std::vector<int> textToBin(const std::string& input) {
     std::vector<int> bits;
 
     for (std::size_t i = 0; i < input.size(); ++i) {
-        // mirror bitset in order to get the right direction. Don't ask me why c++ is flipping the bitset when iterating over it
         auto convertedBits = std::bitset<8>(input[i]);
+        // using a reversed for loop in order to get the right direction. Don't ask me why c++ is flipping the bitset when iterating over it
         for (int ii = convertedBits.size() - 1; ii >= 0; --ii) {
             bits.push_back(convertedBits[ii]);
         }
@@ -39,33 +39,42 @@ char binToText(std::string input) {
     return temp;
 }
 
-void leastSignificantBitEncode(std::string imagePath, std::string text) {
+void leastSignificantBitEncode(std::string imagePath, std::string input) {
     cv::Mat img = cv::imread(imagePath);
 
     // check how many pixels must be manipulated to fit the text
-    int maxTextNum = text.size() * 8;
-
+    int maxTextNum = input.size() * 8;
     int neededPixels = (maxTextNum % 3 == 0) ? maxTextNum / 3 : maxTextNum / 3 + 1;
 
-    std::vector<int> binConvertedText = textToBin(text);
+    // fetch the binary numbers of the input text
+    std::vector<int> binConvertedText = textToBin(input);
     for (auto binConv : binConvertedText) {
         std::cout << binConv;
     }
     int pixelCounter = 0;
+    // counts at which bit we are
     int counter = 0;
+    // iterate over image pixels
     for (int i = 0; i < img.rows; i++) {
+        // if number of needed pixels is reached stop iterating
+        if (pixelCounter > neededPixels) {
+            break;
+        }
         for (int j = 0; j < img.cols; j++) {
             // if number of needed pixels is reached stop iterating
             if (pixelCounter > neededPixels) {
                 break;
             }
+            // fetch RGB values of pixel
             cv::Vec3b& intensity = img.at<cv::Vec3b>(i, j);
             for (int k = 0; k < img.channels(); k++) {
+                // convert color value to binary number
                 std::string binConvertedValue = decToBin(int(intensity.val[k]));
 
+                // manipulate last bit of binary number
                 binConvertedValue.replace(7, 1, std::to_string(binConvertedText[counter]));
 
-                // convert RGB values to binary and change last bit with text bit
+                // change out color value
                 intensity.val[k] = binToDec(binConvertedValue);
                 ++counter;
             }
