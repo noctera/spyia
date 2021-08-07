f657e1efdc5c5169995bd754e44da555d07be665
noctera <julian.jaksch@gmx.de>
2021-08-07 23:49:00 +0200

    improved text to binary test cases

--- tests/TextToBin.cpp
+++ tests/TextToBin.cpp
@@ -1,48 +1,54 @@
 #include "CppSteg/steganography.hpp"
 #include <catch2/catch.hpp>
+#include <map>
 #include <string>
 #include <vector>
 
-TEST_CASE("Check if conversions from ASCII letters to binary are successful", "[textToBin]") {
-    REQUIRE(steg::textToBin("a") == std::vector<int>{0, 1, 1, 0, 0, 0, 0, 1});
-    REQUIRE(steg::textToBin("b") == std::vector<int>{0, 1, 1, 0, 0, 0, 1, 0});
-    REQUIRE(steg::textToBin("c") == std::vector<int>{0, 1, 1, 0, 0, 0, 1, 1});
-    REQUIRE(steg::textToBin("d") == std::vector<int>{0, 1, 1, 0, 0, 1, 0, 0});
-    REQUIRE(steg::textToBin("e") == std::vector<int>{0, 1, 1, 0, 0, 1, 0, 1});
-    REQUIRE(steg::textToBin("f") == std::vector<int>{0, 1, 1, 0, 0, 1, 1, 0});
-    REQUIRE(steg::textToBin("g") == std::vector<int>{0, 1, 1, 0, 0, 1, 1, 1});
-    REQUIRE(steg::textToBin("h") == std::vector<int>{0, 1, 1, 0, 1, 0, 0, 0});
-    REQUIRE(steg::textToBin("i") == std::vector<int>{0, 1, 1, 0, 1, 0, 0, 1});
-    REQUIRE(steg::textToBin("j") == std::vector<int>{0, 1, 1, 0, 1, 0, 1, 0});
-    REQUIRE(steg::textToBin("k") == std::vector<int>{0, 1, 1, 0, 1, 0, 1, 1});
-    REQUIRE(steg::textToBin("l") == std::vector<int>{0, 1, 1, 0, 1, 1, 0, 0});
-    REQUIRE(steg::textToBin("m") == std::vector<int>{0, 1, 1, 0, 1, 1, 0, 1});
-    REQUIRE(steg::textToBin("n") == std::vector<int>{0, 1, 1, 0, 1, 1, 1, 0});
-    REQUIRE(steg::textToBin("o") == std::vector<int>{0, 1, 1, 0, 1, 1, 1, 1});
-    REQUIRE(steg::textToBin("p") == std::vector<int>{0, 1, 1, 1, 0, 0, 0, 0});
-    REQUIRE(steg::textToBin("q") == std::vector<int>{0, 1, 1, 1, 0, 0, 0, 1});
-    REQUIRE(steg::textToBin("r") == std::vector<int>{0, 1, 1, 1, 0, 0, 1, 0});
-    REQUIRE(steg::textToBin("s") == std::vector<int>{0, 1, 1, 1, 0, 0, 1, 1});
-    REQUIRE(steg::textToBin("t") == std::vector<int>{0, 1, 1, 1, 0, 1, 0, 0});
-    REQUIRE(steg::textToBin("u") == std::vector<int>{0, 1, 1, 1, 0, 1, 0, 1});
-    REQUIRE(steg::textToBin("v") == std::vector<int>{0, 1, 1, 1, 0, 1, 1, 0});
-    REQUIRE(steg::textToBin("w") == std::vector<int>{0, 1, 1, 1, 0, 1, 1, 1});
-    REQUIRE(steg::textToBin("x") == std::vector<int>{0, 1, 1, 1, 1, 0, 0, 0});
-    REQUIRE(steg::textToBin("y") == std::vector<int>{0, 1, 1, 1, 1, 0, 0, 1});
-    REQUIRE(steg::textToBin("z") == std::vector<int>{0, 1, 1, 1, 1, 0, 1, 0});
-    REQUIRE(steg::textToBin("A") == std::vector<int>{0, 1, 0, 0, 0, 0, 0, 1});
-    REQUIRE(steg::textToBin("B") == std::vector<int>{0, 1, 0, 0, 0, 0, 1, 0});
-    REQUIRE(steg::textToBin("C") == std::vector<int>{0, 1, 0, 0, 0, 0, 1, 1});
-    REQUIRE(steg::textToBin("D") == std::vector<int>{0, 1, 0, 0, 0, 1, 0, 0});
-    REQUIRE(steg::textToBin("E") == std::vector<int>{0, 1, 0, 0, 0, 1, 0, 1});
-    REQUIRE(steg::textToBin("F") == std::vector<int>{0, 1, 0, 0, 0, 1, 1, 0});
-    REQUIRE(steg::textToBin("G") == std::vector<int>{0, 1, 0, 0, 0, 1, 1, 1});
-    REQUIRE(steg::textToBin("H") == std::vector<int>{0, 1, 0, 0, 1, 0, 0, 0});
-    REQUIRE(steg::textToBin("I") == std::vector<int>{0, 1, 0, 0, 1, 0, 0, 1});
-    REQUIRE(steg::textToBin("J") == std::vector<int>{0, 1, 0, 0, 1, 0, 1, 0});
-    REQUIRE(steg::textToBin("K") == std::vector<int>{0, 1, 0, 0, 1, 0, 1, 1});
-    REQUIRE(steg::textToBin("L") == std::vector<int>{0, 1, 0, 0, 1, 1, 0, 0});
-    REQUIRE(steg::textToBin("M") == std::vector<int>{0, 1, 0, 0, 1, 1, 0, 1});
+TEST_CASE("Check lowercase ASCII letters", "[textToBin]") {
+    std::map<std::string, std::string> letterBinaryDict{{"a", "01100001"}, {"b", "01100010"}, {"c", "01100011"},
+                                                        {"d", "01100100"}, {"e", "01100101"}, {"f", "01100110"},
+                                                        {"g", "01100111"}, {"h", "01101000"}, {"i", "01101001"},
+                                                        {"j", "01101010"}, {"k", "01101011"}, {"l", "01101100"},
+                                                        {"m", "01101101"}, {"n", "01101110"}, {"o", "01101111"},
+                                                        {"p", "01110000"}, {"q", "01110001"}, {"r", "01110010"},
+                                                        {"s", "01110011"}, {"t", "01110100"}, {"u", "01110101"},
+                                                        {"v", "01110110"}, {"w", "01110111"}, {"x", "01111000"},
+                                                        {"y", "01111001"}, {"z", "01111010"}};
+
+    for (auto const& [key, value] : letterBinaryDict) {
+        std::vector<int> tempBinVec;
+
+        for (int i = 0; i < 8; ++i) {
+            tempBinVec.push_back(value[i] - 48);
+        }
+
+        REQUIRE(steg::textToBin(key) == tempBinVec);
+    }
+
+}
+
+TEST_CASE("Check upercase ASCII letters", "[textToBin]") {
+    std::map<std::string, std::string> letterBinaryDict{{"A", "01000001"}, {"B", "01000010"}, {"C", "01000011"},
+                                                        {"D", "01000100"}, {"E", "01000101"}, {"F", "01000110"},
+                                                        {"G", "01000111"}, {"H", "01001000"}, {"I", "01001001"},
+                                                        {"J", "01001010"}, {"K", "01001011"}, {"L", "01001100"},
+                                                        {"M", "01001101"}, {"N", "01001110"}, {"O", "01001111"},
+                                                        {"P", "01010000"}, {"Q", "01010001"}, {"R", "01010010"},
+                                                        {"S", "01010011"}, {"T", "01010100"}, {"U", "01010101"},
+                                                        {"V", "01010110"}, {"W", "01010111"}, {"X", "01011000"},
+                                                        {"Y", "01011001"}, {"Z", "01011010"}};
+
+    for (auto const& [key, value] : letterBinaryDict) {
+        std::vector<int> tempBinVec;
+
+        for (int i = 0; i < 8; ++i) {
+            tempBinVec.push_back(value[i] - 48);
+        }
+
+        REQUIRE(steg::textToBin(key) == tempBinVec);
+    }
+}
+    /*
     REQUIRE(steg::textToBin("N") == std::vector<int>{0, 1, 0, 0, 1, 1, 1, 0});
     REQUIRE(steg::textToBin("O") == std::vector<int>{0, 1, 0, 0, 1, 1, 1, 1});
     REQUIRE(steg::textToBin("P") == std::vector<int>{0, 1, 0, 1, 0, 0, 0, 0});
@@ -63,5 +69,4 @@ TEST_CASE("Check if conversions from ASCII letters to binary are successful", "[
     REQUIRE(steg::textToBin("Z") == std::vector<int>{00100000});
     REQUIRE(steg::textToBin("Z") == std::vector<int>{00100000});
     REQUIRE(steg::textToBin("Z") == std::vector<int>{00100000});
-    REQUIRE(steg::textToBin("Z") == std::vector<int>{00100000});
-}
+    REQUIRE(steg::textToBin("Z") == std::vector<int>{00100000});*/
