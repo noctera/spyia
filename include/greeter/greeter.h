0cc41bb4164d548744afa6474f8fb0c924b3ac3b
noctera <noctera.dev@gmail.com>
2022-02-22 19:00:27 +0100

    refactor: new basic cmake setup

---
+++ include/greeter/greeter.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <string>
+#include <fmt/format.h>
+
+namespace greeter {
+
+  /**  Language codes to be used with the Greeter class */
+  enum class LanguageCode { EN, DE, ES, FR };
+
+  /**
+   * @brief A class for saying hello in multiple languages
+   */
+  class Greeter {
+    std::string name;
+
+  public:
+    /**
+     * @brief Creates a new greeter
+     * @param name the name to greet
+     */
+    Greeter(std::string _name) : name(std::move(_name)) {};
+
+    /**
+     * @brief Creates a localized string containing the greeting
+     * @param lang the language to greet in
+     * @return a string containing the greeting
+     */
+    std::string greet(LanguageCode lang = LanguageCode::EN) const {
+      switch (lang) {
+    default:
+    case LanguageCode::EN:
+      return fmt::format("Hello, {}!", name);
+    case LanguageCode::DE:
+      return fmt::format("Hallo {}!", name);
+    case LanguageCode::ES:
+      return fmt::format("¡Hola {}!", name);
+    case LanguageCode::FR:
+      return fmt::format("Bonjour {}!", name);
+  }
+    };
+  };
+
+}  // namespace greeter
