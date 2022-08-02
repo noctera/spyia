71e31a4d54d5db5aea97248fe5c4efbeb3cbf6bf
noctera <noctera.dev@gmail.com>
2022-08-02 23:57:04 +0200

    feat: basic types

---
+++ include/spyia/fileTypes/fileTypes.hpp
@@ -0,0 +1,32 @@
+#pragma once
+
+namespace Spyia
+{
+    // all different file types
+    enum class FileTypes
+    {
+        JPEG,
+        GIF,
+        SVG,
+        PNG,
+        TIF,
+        MP4,
+        AVI,
+        MOV,
+        FLV,
+        AVCHD,
+        M4A,
+        MP3,
+        WAV,
+        PDF,
+        DOC,
+        DOCX,
+        HTML,
+        XLS,
+        TXT,
+        PPT,
+        PPTX,
+        ODP,
+        KEY
+    };
+}
---
+++ include/spyia/steganography/stegTypes.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+namespace Spyia
+{
+    enum class StegTypes
+    {
+        LSB
+    };
+}
