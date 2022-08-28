2621c1548c2e09a0426fee332c18f049fa0e864e
noctera <noctera.dev@proton.me>
2022-08-28 12:38:08 +0200

    feat: enum to string conversions

--- include/spyia/utils/utils.hpp
+++ include/spyia/utils/utils.hpp
@@ -39,10 +39,40 @@ namespace Spyia {
         return numbers;
     }
 
-    inline std::string encryptionToString(Encryption::EncryptionType encryptionType) {
+    inline std::string encryptionTypeToString(Encryption::EncryptionType encryptionType)
+    {
         switch(encryptionType) {
-            case Encryption::EncryptionType::AES_CBC: return "AESCBC";
-            case Encryption::EncryptionType::NONE: return "NONE";
+            case Encryption::EncryptionType::AES_CBC: return "AESCBC"; break;
+            case Encryption::EncryptionType::NONE: return "NONE"; break;
+        }
+    }
+
+    inline std::string fileTypeToString(File::FileType fileType)
+    {
+        switch(fileType) {
+            case File::FileType::JPEG: return "JPEG"; break;
+            case File::FileType::GIF: return "GIF"; break;
+            case File::FileType::SVG: return "SVG"; break;
+            case File::FileType::PNG: return "PNG"; break;
+            case File::FileType::TIF: return "TIF"; break;
+            case File::FileType::MP4: return "MP4"; break;
+            case File::FileType::AVI: return "AVI"; break;
+            case File::FileType::MOV: return "MOV"; break;
+            case File::FileType::FLV: return "FLV"; break;
+            case File::FileType::AVCHD: return "AVCHD"; break;
+            case File::FileType::M4A: return "M4A"; break;
+            case File::FileType::MP3: return "MP3"; break;
+            case File::FileType::WAV: return "WAV"; break;
+            case File::FileType::PDF: return "PDF"; break;
+            case File::FileType::DOC: return "DOC"; break;
+            case File::FileType::DOCX: return "DOCX"; break;
+            case File::FileType::HTML: return "HTML"; break;
+            case File::FileType::XLS: return "XLS"; break;
+            case File::FileType::TXT: return "TXT"; break;
+            case File::FileType::PPT: return "PPT"; break;
+            case File::FileType::PPTX: return "PPTX"; break;
+            case File::FileType::ODP: return "ODP"; break;
+            case File::FileType::KEY: return "KEY"; break;
         }
     }
 }
